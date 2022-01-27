69ead2d911ec540674b9cad51a502d09b007b2a1
ThiruDev50 <thiru.dev50@gmail.com>
2022-01-27 22:55:14 +0530

    Login page Navbar aaded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginRegisterComponent } from './login-register/login-register.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { TestComponent } from './test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'test', component:TestComponent},
+{path:'',component:LandingPageComponent},
+{path:'Login',component:LoginRegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
